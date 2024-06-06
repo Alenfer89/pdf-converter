bf4bd6997fe523b4e310740fbf6dfb3e67afbbb3
Alex Alberio <alberioalex@gmail.com>
2024-06-06 10:30:44 +0200

    revamped structure

--- config/local_config.php
+++ config/local_config.php
@@ -1,11 +1,6 @@
 <?php
 
 return [
-    // 'scan_path' => str_replace('\\', '/', __DIR__ . '..\scan'),
-    // 'tmp_path' => str_replace('\\', '/', __DIR__ . '..\manage'),
-    // 'output_path' => str_replace('\\', '/', __DIR__ . '..\output'),
-    // 'error_path' => str_replace('\\', '/', __DIR__ . '..\error'),
-
     //remote (o anche no)
     'scan_path'          => realpath('scan'),
     'error_path'         => realpath('error'),
